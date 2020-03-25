cddefc1b9d8e793397c53bf444b702805d138e02
Matthew McCoy <mmccoy23@uncc.edu>
2020-03-25 19:59:00 -0400

    Added a backend and created a local express server to host the landing page

---
+++ public/app.js
@@ -0,0 +1,19 @@
+//require the express library
+const express = require('express');
+//initialize a new application
+const app = express();
+//set up a port
+const port = process.env.PORT || 8080;
+
+//specifying the static route
+app.use('/assets', express.static('assets'));
+
+//default route for the landing page
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/views/landing.html');
+});
+
+//initialize and listen on a port
+app.listen(port, () => {
+    console.log(`App has started and is listening on port ${port}`);
+});
