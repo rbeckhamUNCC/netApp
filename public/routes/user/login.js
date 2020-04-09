16b848053dd564076ad1497e6e4ae2ce3faca355
crenfrow <crenfrow@uncc.edu>
2020-04-08 21:30:39 -0400

    removed deprecated error, was line 5 in login.js

--- public/routes/user/login.js
+++ public/routes/user/login.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 var User = require("../../models/user");
-router.use(bodyParser.urlencoded());
 router.use(bodyParser.urlencoded({extended: true}));
 
 //   /user/login
