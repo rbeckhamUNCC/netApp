62f18cd98f2a99e2f9d260953e8129375a066be7
crenfrow <crenfrow@uncc.edu>
2020-04-09 20:55:39 -0400

    Revert "removed deprecated error, was line 5 in login.js"
    This reverts commit 16b848053dd564076ad1497e6e4ae2ce3faca355.

--- public/routes/user/login.js
+++ public/routes/user/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 var User = require("../../models/user");
+router.use(bodyParser.urlencoded());
 router.use(bodyParser.urlencoded({extended: true}));
 
 //   /user/login
