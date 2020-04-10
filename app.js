077ce2cc6c65ed37274dbe20ae54a67cfc405f27
crenfrow <crenfrow@uncc.edu>
2020-04-10 12:01:29 -0400

    added login route directly to app.js to see if routing is the issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('./node_modules/express');
 var path = require('path');
 var app = express();
 
-require("./public/routes")(app);
+//require("./public/routes")(app);
 
 var logger = require('./node_modules/morgan');
 var cookieParser = require('./node_modules/cookie-parser');
@@ -96,6 +96,31 @@ app.get('/MyAvailability', function(request, response) {
 
 app.get('/MyAccount', function(request, response) {
     response.sendFile(path.join(__dirname + '/public/views/MyAccount.html'));
+});
+
+app.post('/login',function(request,response){
+    console.log("/login reached:" + request.body.email)
+    var email = request.body.email;
+    var password = request.body.password;
+    //specify user to find by email and password
+    User.findOne({email: email, password: password}, function(err,user){
+         //user doesnt exist
+       if(!user) {
+           return response.status(404).send(`incorrect credentials!`);
+       } //user exists
+
+       if(err) {
+           //error found, log error
+           console.log(err);
+           return response.status(500).send(`incorrect credentials!`);
+       }
+      
+       //set logged in user session
+       // below needs to be uncommented so that sessions can be set, maybe this needs to be put somewhere else
+       //request.session.user = user;
+       // after logged in
+       return response.status(200).send(`${email} logged in!`);
+    });
 });
 
 //initialize and listen on a port
